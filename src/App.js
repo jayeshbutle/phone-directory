13963ae13390f8d299f438fcd5452068a40d1054
jayeshbutle <jayeshbutle39@gmail.com>
2021-12-08 11:25:23 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Fragment } from 'react';
+
 function App() {
   return (
-    <div>
-      Phone Directory
-    </div>
-  );
+    <Fragment>
+      <div>
+        Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <span>Phone</span>
+      </div>
+    </Fragment>
+  )
 }
 
 export default App;
