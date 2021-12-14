3b187828ab079be65fa99f752eceee4b7db0af98
jayeshbutle <jayeshbutle39@gmail.com>
2021-12-14 11:29:37 +0530

    Styled ‘Header’ (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./header.css"
 
-// const Header = function () {
+const Header = function () {
 //   const headerStyle = {
 //     textAlign: 'center',
 //     padding: 20,
